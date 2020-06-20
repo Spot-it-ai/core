48eb54fc52a7a51d0aeb69b68d0ab4d075ee3410
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2020-06-20 14:38:46 -0400

    [FIX] CORS issue

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: { origin: ["http://127.0.0.1:3000"] } });
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: { origin: ["http://127.0.0.1:3000", "https://spot-it.surge.sh"] } });
     await app.listen((process.env.PORT || 3001));
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    { cors: { origin: ["http://127.0.0.1:3000"] }}
+    { cors: { origin: ["http://127.0.0.1:3000", "https://spot-it.surge.sh"] }}
   );
   await app.listen(( process.env.PORT || 3001 ));
 }
